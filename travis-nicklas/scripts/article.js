fea22392ec4f2f06d006c5f60ef3d1798edb70db
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-06-08 13:21:58 -0700

    Most things complete. Stretch goal to be attempted.

--- travis-nicklas/scripts/article.js
+++ travis-nicklas/scripts/article.js
@@ -13,7 +13,7 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.  Done
-  let source = $("#article-template").html();
+  let template = Handlebars.compile($('#article-template').html());
   
   // REVIEW: If your template will use properties that aren't on the object yet, add them.  Done
   // Since your template can't hold any JS logic, we need to execute the logic here.
@@ -29,10 +29,9 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-  let templateRender =Handlebars.compile(source);
-  return templateRender(this);
-
+  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article. DONE
+  // let templateRender = Handlebars.compile(source);
+  return template(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
--- travis-nicklas/scripts/articleView.js
+++ travis-nicklas/scripts/articleView.js
@@ -2,21 +2,21 @@
 
 let articleView = {};
 
-// TODO: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom.
+// TODO: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom. DONE
 
 // COMMENT: How do arrow functions affect the context of "this"? How did you determine if a function could be refactored?
-// PUT YOUR RESPONSE HERE
+// Arrow functions do not change contextual this, where as conventional functions change contextual this to be the function. Determination was chosen based off the contextual this. If .this was used, we chose to not change anything and leave as is. The other half of the time guess and check. Live server is your friend!
 
-articleView.populateFilters = function() {
+articleView.populateFilters = () => {
   $('article').each(function() {
     if (!$(this).hasClass('template')) {
-      let val = $(this).find('address a').text();
+      let val = $(this).attr('data-author');
       let optionTag = `<option value="${val}">${val}</option>`;
 
       if ($(`#author-filter option[value="${val}"]`).length === 0) {
         $('#author-filter').append(optionTag);
       }
-
+      
       val = $(this).attr('data-category');
       optionTag = `<option value="${val}">${val}</option>`;
       if ($(`#category-filter option[value="${val}"]`).length === 0) {
@@ -26,7 +26,7 @@ articleView.populateFilters = function() {
   });
 };
 
-articleView.handleAuthorFilter = function() {
+articleView.handleAuthorFilter = () => {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
@@ -39,7 +39,7 @@ articleView.handleAuthorFilter = function() {
   });
 };
 
-articleView.handleCategoryFilter = function() {
+articleView.handleCategoryFilter = () => {
   $('#category-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
@@ -52,7 +52,7 @@ articleView.handleCategoryFilter = function() {
   });
 };
 
-articleView.handleMainNav = function() {
+articleView.handleMainNav = () => {
   $('nav').on('click', '.tab', function(e) {
     e.preventDefault();
     $('.tab-content').hide();
@@ -62,7 +62,7 @@ articleView.handleMainNav = function() {
   $('nav .tab:first').click();
 };
 
-articleView.setTeasers = function() {
+articleView.setTeasers = () => {
   $('.article-body *:nth-of-type(n+2)').hide();
   $('article').on('click', 'a.read-on', function(e) {
     e.preventDefault();
